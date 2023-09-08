c851b18db30ea11919940ad4bc3cfe336ea35b6c
Manpreet Singh <110181810+lucifermani21@users.noreply.github.com>
2023-09-08 16:39:12 +0530

    Update custom_extra_setting.php

--- plugin-templates/custom_extra_setting.php
+++ plugin-templates/custom_extra_setting.php
@@ -3,17 +3,17 @@ $settings = array(
 	array(
 		'title' => __( 'Extra CSS', 'woocommerce-custom-settings-plugin' ),
 		'type'  => 'title',
-		'desc'  => '',
+		'desc'  => 'Coming Soon...',
 		'id'    => 'extra_options',
 	),
-	array(
+	/*array(
 		'title' => __( 'Add Custom Style', 'text-domain' ),
 		'type' => 'ms_css_editor',
 		'desc' => __( 'Add custom CSS here for WooCoomerce or overwrite current CSS.', 'text-domain' ),
 		'desc_tip' => true,
 		'id' => 'custom_woocommerce_css',
 		'css' => 'width:100%;height:300px;',
-	),
+	),*/
 	array(
 		'type' => 'sectionend',
 		'id'   => 'extra_options',
