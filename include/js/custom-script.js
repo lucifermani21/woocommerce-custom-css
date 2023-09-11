1e85fed43a641913c0a2bdcb35b7f7e5c501b91d
Manpreet Singh <110181810+lucifermani21@users.noreply.github.com>
2023-09-11 08:53:28 +0530

    Update custom-script.js

--- include/js/custom-script.js
+++ include/js/custom-script.js
@@ -1,1 +1,12 @@
 /* Custom Script Codes */
+(function ($) {
+    var codeMirror = CodeMirror.fromTextArea(document.querySelector(".codemirror_text"), {
+        'mode': 'htmlmixed',
+        'lineNumbers': true,
+        'lineWrapping': true,
+        'indentUnit': 0,
+        'cm-tab-extra-space-remove': true,					
+    });
+    console.log(codeMirror);
+    codeMirror.focus();
+})(jQuery);
