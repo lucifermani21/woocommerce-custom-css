9f1206775781ef8deebe648ea1311202be58538c
lucifermani21 <manpreetsinghmanidev@gmail.com>
2023-09-12 12:42:18 +0530

    script code done

--- include/js/custom-admin-script.js
+++ include/js/custom-admin-script.js
@@ -1,15 +1,16 @@
 /* Custom Admin Script Codes */
 (function ($) {
-    //let textarea_count = 0;
-    //let forminptextarea =  document.querySelectorAll( '.forminp-textarea textarea' );
-    //console.log(forminptextarea);
-    var codeMirror = CodeMirror.fromTextArea(document.querySelector(".forminp-textarea textarea"), {
-        'mode': 'htmlmixed',
-        'lineNumbers': true,
-        'lineWrapping': true,
-        'indentUnit': 0,
-        'cm-tab-extra-space-remove': true,					
-    });
-    //console.log(codeMirror);
-    codeMirror.focus();
+    let forminptextarea =  document.querySelectorAll( 'textarea' );
+    forminptextarea.forEach((item) => {
+        if (item.className === 'codemirror_text') {
+            var codeMirror = CodeMirror.fromTextArea( item, {
+                'mode': 'htmlmixed',
+                'lineNumbers': true,
+                'lineWrapping': true,
+                'indentUnit': 0,
+                'cm-tab-extra-space-remove': true,					
+            });
+            codeMirror.focus();
+        }
+    })    
 })(jQuery);
