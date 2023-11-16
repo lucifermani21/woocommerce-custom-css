2ee9c8d4dcb3e96653190dc2af83d6172d17178f
Manpreet Singh <110181810+lucifermani21@users.noreply.github.com>
2023-11-16 13:13:07 +0530

    Update woocommerce-custom-css.php
    changes

--- woocommerce-custom-css.php
+++ woocommerce-custom-css.php
@@ -3,15 +3,14 @@
 * Plugin Name: WooCommerce Custom Theme Options
 * Plugin URI: https://github.com/lucifermani21/woocommerce-custom-css.git
 * Description: WooCommerce custom theme CSS variables with SASS and another pages edit options with WooCommerce Support. Product Image Hover Effect add option and Quick View option avilable.
-* Version: 3.0.2
+* Version: 2.5.2
 * Author: Manpreet Singh
-* Author URI: https://github.com/lucifermani21/woocommerce-custom-css.git
 **/
 
 if ( ! defined( 'ABSPATH' ) ) {
      die;
 }
-define( 'CUSTOM_SETTING_VERSION', '2.5.1' );
+define( 'CUSTOM_SETTING_VERSION', '2.5.2' );
 define( 'CUSTOM_SETTING_TEXT_DOMAIN', 'woocommerce-custom-css' );
 define( 'CUSTOM_DIR__NAME', dirname( __FILE__ ) );
 define( 'CUSTOM_EDITING__URL', plugin_dir_url( __FILE__ ) );
@@ -19,10 +18,6 @@ define( 'CUSTOM_EDITING__DIR', plugin_dir_path( __FILE__ ) );
 define( 'CUSTOM_SETTING_PLUGIN', __FILE__ );
 define( 'CUSTOM_SETTING_PLUGIN_BASENAME', plugin_basename( CUSTOM_SETTING_PLUGIN ) );
 
-if ( ! function_exists( 'is_woocommerce_activated' ) ) {
-	function is_woocommerce_activated() {
-		if ( class_exists( 'woocommerce' ) ) 
-		{
 include_once CUSTOM_EDITING__DIR .  '/include/class-WOO-CSS.php';
 include_once CUSTOM_EDITING__DIR .  '/include/plugin_setting_class.php';
 include_once CUSTOM_EDITING__DIR .  '/include/plugin_custom_functions.php';
@@ -30,7 +25,3 @@ include_once CUSTOM_EDITING__DIR .  '/include/plugin_hooks_setting.php';
 include_once CUSTOM_EDITING__DIR .  '/include/plugin_css_setting.php';
 include_once CUSTOM_EDITING__DIR .  '/include/plugin_hover_image.php';
 //include_once CUSTOM_EDITING__DIR .  '/include/plugin_product_quickview.php';
-		}
-	}
-}
-is_woocommerce_activated();
