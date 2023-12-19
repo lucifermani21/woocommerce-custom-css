d7cd17a4be7438c8460492943534c32345b3ce25
Manpreet Singh <manpreetsinghmanidev@gmail.com>
2023-12-19 21:47:12 +0530

    Sale counter added

--- include/plugin_sale_counter.php
+++ include/plugin_sale_counter.php
@@ -11,7 +11,7 @@ class WOO_SALE_COUNTER
 	{
 		add_action( 'wp_enqueue_scripts', array( $this, 'MS_sale_counter_scripts' ) );
 		add_action('woocommerce_before_add_to_cart_form', array( $this, 'MS_sale_counter_shop_detail_page' ) );
-		//add_action( 'woocommerce_after_shop_loop_item', array( $this, 'MS_sale_loop_shop_page' ), 10 );
+		add_action( 'woocommerce_after_shop_loop_item', array( $this, 'MS_sale_loop_shop_page' ), 10 );
 		
 	}
 	public function MS_sale_counter_scripts()
@@ -29,6 +29,25 @@ class WOO_SALE_COUNTER
 	}
 	
 	public function MS_sale_counter_shop_detail_page(){
+		global $product;
+		$sale_start_date = get_post_meta( $product->get_id(), '_sale_price_dates_from', true );
+		$sale_end_date = get_post_meta( $product->get_id(), '_sale_price_dates_to', true );		
+		$date_format = apply_filters( 'woocommerce_sale_price_end_date_format', 'M j, Y' );
+		$start_date = date_i18n( $date_format, $sale_start_date );
+        $end_date = date_i18n( $date_format, $sale_end_date );
+		$end_time = ( $sale_start_date && $sale_end_date ) != '' ? '23:59:00' : '00:00:00';
+		$current_date = date( 'M j, Y' );
+		//var_dump( $sale_end_date );
+		if( ( !empty( $start_date ) ) && ( !empty( $end_date ) ) ){
+			if( ( $start_date != $current_date ) && !empty( $start_date ) && ( $end_date != $current_date ) && ( $start_date != $end_date ) && ( $sale_start_date != '' ) ){
+				$this->sale_starttimer_html( $start_date, $end_time );
+			}elseif( ( $sale_end_date ) != '' ){
+				$this->sale_endtimer_html( $end_date, $end_time );
+			}
+		}
+	}
+
+	public function MS_sale_loop_shop_page(){
 		global $product;
 		$sale_start_date = get_post_meta( $product->get_id(), '_sale_price_dates_from', true );
 		$sale_end_date = get_post_meta( $product->get_id(), '_sale_price_dates_to', true );		
